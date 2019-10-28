2fcd10c8edabb71b27065019c01d8c5195d93b7a
Jeferson.Rocha <tecjeferson@gmail.com>
2019-10-28 20:50:27 -0300

    CRUD Finalized and working

--- LojaAutoPecas/App_Start/RouteConfig.cs
+++ LojaAutoPecas/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace LojaAutoPecas
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Pecas", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
